72bf5293ecbf5e131fb136cf288cbd0c8a8527f9
xdanu <danu.kallio@gmail.com>
2026-09-10 21:14:16 +0300

    show1 lisäys

--- leds.cpp
+++ leds.cpp
@@ -35,16 +35,103 @@ void setAllLeds()
 }
 
 
-void show1()    //kesken
-{
-    for (int i = 0; i < 15; i ++){
-        
-    }
+void show1()    //näyttää 0 -> 15 binääriluvuin ledeillä
+{   //tähän vois myös laittaa jonkun while loopin jos tarvii
+    for (int i = 0; i <= 15; i ++){
 
-    
+        clearAllLeds(); //pyyhitään ledit aina ennen uuden binääriluvun näyttöä
+        delay(200); //200ms viive ennen seuraavan luvun näyttöä
+
+        switch (i){ //for-silmukka määrää casen
+
+        case 0: //0000, kaikki ledit pois päältä
+        break; //poistutaan switchistä
+
+        case 1: //0001
+        setLed(0); //sytytetään led 0
+        break;
+
+        case 2: //0010
+        setLed(1);
+        break;
+
+        case 3: //0011
+        setLed(0);
+        setLed(1);
+        break;
+
+        case 4: //0100
+        setLed(2);
+        break;
+
+        case 5: //0101
+        setLed(0);
+        setLed(2);
+        break;
+
+        case 6: //0110
+        setLed(1);
+        setLed(2);
+        break;
+
+        case 7: //0111
+        setLed(0);
+        setLed(1);
+        setLed(2);
+        break;
+
+        case 8: //1000
+        setLed(3);
+        break;
+
+        case 9: //1001
+        setLed(0);
+        setLed(3);
+        break;
+
+        case 10: //1010
+        setLed(1);
+        setLed(3);
+        break;
+
+        case 11: //1011
+        setLed(0);
+        setLed(1);
+        setLed(3);
+        break;
+
+        case 12: //1100
+        setLed(2);
+        setLed(3);
+        break;
+
+        case 13: //1101
+        setLed(0);
+        setLed(2);
+        setLed(3);
+        break;
+
+        case 14: //1110
+        setLed(1);
+        setLed(2);
+        setLed(3);
+        break;
+
+        case 15: //1111
+        setLed(0);
+        setLed(1);
+        setLed(2);
+        setLed(3);
+        break;
+
+        }
+        delay(500); //näytetään jokaista lukua 500ms
+    }
+    clearAllLeds(); //valot nollataan for-loopin jälkeen
+    delay(2000); //odotetaan 2s ennen kuin seuraava show voi alkaa
 }
 
-void show2(int rounds)
+void show2(int rounds) //vilkuttaa ledejä 0, 1, 2, 3 kiihtyvällä tahdilla
 {
     int kaydytkierrokset = 0;
     int odotusaika = 1000;
