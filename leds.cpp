0051d76bd183e81334c9440489c90ca209d64542
xdanu <danu.kallio@gmail.com>
2026-09-24 16:38:28 +0300

    show1 PORTC kikkailulla

--- leds.cpp
+++ leds.cpp
@@ -32,105 +32,29 @@ void setAllLeds()
 
 void show1()    //näyttää 0 -> 15 binääriluvuin ledeillä
 {   
+    while(!gameOn){
 
-    
-    
-    
-    //..-.ö.
-    
-    /*//tähän vois myös laittaa jonkun while loopin jos tarvii
-    for (int i = 0; i <= 15; i ++){
+        for(int luku = 0; luku <=15; luku++){ //luku on näytettävä numero desimaalilukuna
 
-        clearAllLeds(); //pyyhitään ledit aina ennen uuden binääriluvun näyttöä
-        delay(200); //200ms viive ennen seuraavan luvun näyttöä
+            if(gameOn){
+                //kun peli alkaa, nollataan 00111100 bitit ja poistutaan showsta
+                PORTC &= ~((1 << PC2) | (1 << PC3) | (1 << PC4) | (1 << PC5));
+                return;
+            }
 
-        switch (i){ //for-silmukka määrää casen
+            //laitetaan PORTC maskiksi 00111100 ---> '~' eli nollataan PC2, PC3, PC4, PC5. nää bitit vastaa pinnejä 16-19
+            //'luku' bittejä pitää siirtää 2 pykälää (PC0 -> PC2), että se ohjaa oikeita ledejä
+            PORTC = (PORTC & ~((1 << PC2) | (1 << PC3) | (1 << PC4) | (1 << PC5))) | (luku << 2);
+            delay(400); //numero näkyy 400ms
 
-        case 0: //0000, kaikki ledit pois päältä
-        break; //poistutaan switchistä
-
-        case 1: //0001
-        setLed(16); //sytytetään led 0
-        break;
-
-        case 2: //0010
-        setLed(17);
-        break;
-
-        case 3: //0011
-        setLed(16);
-        setLed(17);
-        break;
-
-        case 4: //0100
-        setLed(18);
-        break;
-
-        case 5: //0101
-        setLed(16);
-        setLed(18);
-        break;
-
-        case 6: //0110
-        setLed(17);
-        setLed(18);
-        break;
-
-        case 7: //0111
-        setLed(16);
-        setLed(17);
-        setLed(18);
-        break;
-
-        case 8: //1000
-        setLed(19);
-        break;
-
-        case 9: //1001
-        setLed(16);
-        setLed(19);
-        break;
-
-        case 10: //1010
-        setLed(17);
-        setLed(19);
-        break;
-
-        case 11: //1011
-        setLed(16);
-        setLed(17);
-        setLed(18);
-        break;
-
-        case 12: //1100
-        setLed(18);
-        setLed(19);
-        break;
-
-        case 13: //1101
-        setLed(16);
-        setLed(18);
-        setLed(19);
-        break;
-
-        case 14: //1110
-        setLed(17);
-        setLed(18);
-        setLed(19);
-        break;
-
-        case 15: //1111
-        setLed(16);
-        setLed(17);
-        setLed(18);
-        setLed(19);  
-        break;
+            // nollataan bitit 00111100
+            PORTC &= ~((1 << PC2) | (1 << PC3) | (1 << PC4) | (1 << PC5));
+            delay(200); //pidetään ledejä nollattuna 200ms
 
         }
-        delay(500); //näytetään jokaista lukua 500ms
+        PORTC &= ~((1 << PC2) | (1 << PC3) | (1 << PC4) | (1 << PC5)); //nollataan bitit 00111100 shown jälkeen   
+    
     }
-    clearAllLeds(); //valot nollataan for-loopin jälkeen
-    delay(2000); //odotetaan 2s ennen kuin seuraava show voi alkaa */
 }
 
 void show2(int rounds) //vilkuttaa ledejä 0, 1, 2, 3 kiihtyvällä tahdilla
